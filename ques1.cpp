b1661acf26b2a5531f8556a2f5a58f3560430761
kapildivyanshu5-cpu <kapildivyanshu5@gmail.com>
2026-02-05 22:33:28 +0530

    Add files via upload

---
+++ ques1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+
+int main(){
+int m,n,o,p;
+cout<<"enter the no of student 1" ;
+cin>>n ;
+cout<<"enter the no of student 2" ;
+cin>>m ;
+cout<<"enter the no of student 3" ;
+cin>>o ;
+p=(n+m+o)/3 ;
+cout<<"the average marks of 3 students is "<<p ;
+
+
+
+}
