3c0b0e7795ce1ecc546066155f40c8da24d3cf4b
kapildivyanshu5-cpu <kapildivyanshu5@gmail.com>
2026-02-05 23:13:10 +0530

    Add files via upload

---
+++ ques10.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+int main(){
+/*to find the year is leap or not*/
+int year ;
+cout<<"enter the year" ;
+cin>>year ;
+if(year%4==0){
+
+cout<<"the year  :"<<  year << " is  a leapyear" ;
+
+
+}else cout<<"this is noit leap year" ;
+
+
+}
