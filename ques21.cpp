feea829969ea0c8f2bf41f5452999a28e25a8824
kapildivyanshu5-cpu <kapildivyanshu5@gmail.com>
2026-02-07 23:56:37 +0530

    Add files via upload

---
+++ ques21.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+for(int i=1;i<=4;i++){
+    for(int j=i;j<=4;j++){
+if(i==1){
+cout<<"*";
+}}
+
+}cout<<endl ;
+
+for(int u=1;u<=4;u++){
+
+    for(int y=1;y<=4;y++){
+        if(y==1||y==4){
+
+            cout<<"*";
+
+        }else if(y==2||y==3)
+
+
+            cout<<" ";}cout<<endl;
+    }
+for (int p=1;p<=4;p++){
+
+    for(int j=1;j<=4;j++){
+if(p==4){
+cout<<"*";
+}}}}
